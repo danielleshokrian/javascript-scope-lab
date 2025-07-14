d46ca1883f5bcdb551c440513b6188f193bdc834
Danielle <danielle.shokrian@aol.com>
2025-07-14 15:13:07 -0400

    Created functions to add burgers and milkshake names to menu

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+function addBurger() {
+    const newBurger = 'Flatburger';
+    burgers.push(newBurger);
+}
+
+    if (true) {
+        const anothernewBurger = 'Maple Bacon Burger';
+        burgers.push(anothernewBurger.name);
+    }
+
+    function changeFeaturedDrink() {
+        featuredDrink = 'The JavaShake';
+    }
+
+      
